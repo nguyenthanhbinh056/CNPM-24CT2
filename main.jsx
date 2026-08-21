fc2a7540dc809d22c50a875370679960bf40b874
nguyenthanhbinh056 <nguyenthanhbinh5060@gmail.com>
2026-08-21 11:45:17 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>chào bạn khóa 24CT2 đến với học phần CNPM-DAU</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
